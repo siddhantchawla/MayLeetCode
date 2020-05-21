0a30c4fbd1070ecab613bc434d98e97d062a443a
siddhantchawla <siddhant.chawla9@gmail.com>
2020-05-21 17:57:45 +0000

    Week 3, Ques - 6 added.

---
+++ Week3/KthSmallestElementInBST.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void solve(TreeNode* root,int &k,int &res)
+    {
+        if(root == NULL)
+            return;
+
+        solve(root->left,k,res);
+        k--;
+        if(k==0)
+            res = root->val;
+        solve(root->right,k,res);
+        return;
+    }
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int res;
+        solve(root,k,res);
+        return res;
+    }
+};
